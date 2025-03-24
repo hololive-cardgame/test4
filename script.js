069d1f99d02a4253560ecc5086def2871f27a925
hololive-cardgame <ejo3ru@gmail.com>
2025-03-24 21:16:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -30,38 +30,39 @@ function generateFilterOptions() {
         "其他": new Set()
     };
 
-// 這是用來儲存卡牌名稱的集合
-cardsData.forEach(card => {
-    keywords.add(card.name);
-    types.add(card.type);
-    attributes.add(card.attribute);
-    if (card.tag) {
-        card.tag.split(" / ").forEach(tag => tags.add(tag));
-    }
-    if (card.set) {
-        if (card.set.includes("起始牌組")) {
-            sets["起始牌組"].add(card.set);
-        }else if (card.set.includes("補充包")) {
-            sets["補充包"].add(card.set);
-        }else if (card.set === "配件" || card.set === "PR卡"){
-            sets["其他"].add(card.set);
+    // 這是用來儲存卡牌名稱的集合
+    cardsData.forEach(card => {
+        keywords.add(card.name);
+        types.add(card.type);
+        attributes.add(card.attribute);
+        if (card.tag) {
+            card.tag.split(" / ").forEach(tag => tags.add(tag));
         }
-    }
-});
+        if (card.set) {
+            if (card.set.includes("起始牌組")) {
+                sets["起始牌組"].add(card.set);
+            }else if (card.set.includes("補充包")) {
+                sets["補充包"].add(card.set);
+            }else if (card.set === "配件" || card.set === "PR卡"){
+                sets["其他"].add(card.set);
+            }
+        }
+    });
 
-// 填充關鍵字選項
-keywords.forEach(keyword => {
-    if (keyword) {
-        const li = document.createElement("li");
-        li.textContent = keyword;
-        // li.classList.add("dropdown-item");
-        li.addEventListener("click", () => {
-        document.getElementById("dropdown-selected-keyword").textContent = keyword;
-        filterCards();
-        });
-        keywordMenu.appendChild(li);
-    }
-});
+    // 填充關鍵字選項
+    keywords.forEach(keyword => {
+        if (keyword) {
+            const li = document.createElement("li");
+            li.textContent = keyword;
+            // li.classList.add("dropdown-item");
+            li.addEventListener("click", () => {
+            document.getElementById("dropdown-selected-keyword").textContent = keyword;
+            filterCards();
+            });
+            keywordMenu.appendChild(li);
+        }
+    });
+}
 
 // 設置下拉選單點擊事件
 // 監聽所有下拉選單
